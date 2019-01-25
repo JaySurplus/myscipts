446207ae0a0d2a29925b9e659716bdb570080a3d
JaySurplus <znznbest2004@gmail.com>
2019-01-25 18:29:14 +0800

    Add SSR Backups and cmder_regs

---
+++ openvino_env.bat
@@ -0,0 +1,7 @@
+@echo off
+echo activate [ openvino ] conda environment
+call activate openvino
+echo conda [ openvino ] environment initialized
+
+echo activate [ setupvars.bat ] variables
+C:\Intel\computer_vision_sdk\bin\setupvars.bat
