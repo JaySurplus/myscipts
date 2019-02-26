67b77f16d486535667aef4f6ebbd13e35bbb59ad
JaySurplus <znznbest2004@gmail.com>
2019-02-26 16:33:57 +0800

    Update opencv.sh
    Add pyenv specified settings

--- opencv_openvino/opencv.sh
+++ opencv_openvino/opencv.sh
@@ -2,7 +2,14 @@ opencvVersion="4.0.1"
 echo "Installing opencv-$opencvVersion"
 #./opencv_prerequisites.sh
 #git clone https://github.com/opencv/opencv.git
-rm -rf opencv opencv_contrib
+if [ -d ./opencv ]; then
+    rm -rf ./opencv
+fi
+
+if [ -d ./opencv_contrib ]; then
+    rm -rf ./opencv_contrib
+fi
+
 if [ -f opencv-$opencvVersion.zip ]; then
     unzip opencv-$opencvVersion
     mv opencv-$opencvVersion opencv
@@ -21,6 +28,7 @@ else
     cd ./opencv_contrib
     git checkout $opencvVersion
     cd ..
+fi
 
 cd opencv
 mkdir build
@@ -36,9 +44,17 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D WITH_TBB=ON \
     -D WITH_V4L=ON \
     -D OPENCV_ENABLE_NONFREE=ON \
-    -d OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+    -D PYTHON_EXECUTABLE=/home/jaysurplus/.pyenv/shims/python \
+    -D PYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
+    -D PYTHON3_INCLUDE_DIRS=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
+    -D PYTHON3_INCLUDE_DIRS2=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
+    -D PYTHON3_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
+    -D PYTHON3_PACKAGES_PATH=$(python -c "import site; print(site.getsitepackages()[0])") \
 	-D BUILD_EXAMPLES=ON ..
 
 
 make -j$(nproc)
-make install
+sudo make install
+sudo ldconfig
+
