f1cee34e9e34aadf1eed7d925c1ffeeb472afe49
JaySurplus <znznbest2004@gmail.com>
2019-02-27 10:01:29 +0800

    making opencv.sh perfect
    Add color prompt to opencv.sh

--- opencv_openvino/opencv.sh
+++ opencv_openvino/opencv.sh
@@ -1,7 +1,52 @@
+############################################################################
+#                                                                          #
+#                                                                          #
+#                                                                          #
+#                                                                          #
+#                                                                          #
+#                                                                          #
+#                                                                          #
+############################################################################
+
+# Dare you change this!!!
+if which tput >/dev/null 2>&1; then
+      ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
+# Now you can change!
+
 opencvVersion="4.0.1"
+
+printf "$GREEN$BOLD"
 echo "Installing opencv-$opencvVersion"
-#./opencv_prerequisites.sh
+printf "$NORMAL"
+
+printf ""
+printf "$GREEN"
+echo "Installing opencv prerequisities."
+printf "$NORMAL"
+exit 1
+./opencv_prerequisites.sh
 #git clone https://github.com/opencv/opencv.git
+
+printf "$RED"
+echo "Cleaning up opencv and opencv_contrib directories."
+printf "$NORMAL"
 if [ -d ./opencv ]; then
     rm -rf ./opencv
 fi
@@ -10,6 +55,8 @@ if [ -d ./opencv_contrib ]; then
     rm -rf ./opencv_contrib
 fi
 
+printf "$GREEN"
+echo "Fetching opencv and opencv_contrib source code."
 if [ -f opencv-$opencvVersion.zip ]; then
     unzip opencv-$opencvVersion
     mv opencv-$opencvVersion opencv
@@ -29,7 +76,10 @@ else
     git checkout $opencvVersion
     cd ..
 fi
+printf "$NORMAL"
 
+printf "$GREEN"
+echo "Installing opencv and opencv_contrib to selected python enviorment."
 cd opencv
 mkdir build
 cd build
@@ -45,7 +95,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D WITH_V4L=ON \
     -D OPENCV_ENABLE_NONFREE=ON \
     -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-    -D PYTHON_EXECUTABLE=/home/jaysurplus/.pyenv/shims/python \
+    -D PYTHON_EXECUTABLE=/home/$(whoami)/.pyenv/shims/python \
     -D PYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
     -D PYTHON3_INCLUDE_DIRS=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
     -D PYTHON3_INCLUDE_DIRS2=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
@@ -57,4 +107,6 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 make -j$(nproc)
 sudo make install
 sudo ldconfig
+printf "$NORMAL"
+
 
